a4ef40292e4ded633488ac97fcb2e4bccc016d65
panupong.shippop <phanupong.shippop@gmail.com>
2023-02-25 14:37:57 +0700

    dev login

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ require('./config/database').connect()
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
+const cors = require('cors')
+app.use(cors())
 const verifyToken  = require('./middleware/auth')
 const routes = require('./routes');
 
--- index.js
+++ index.js
@@ -1,32 +1,15 @@
 const http = require('http')
 const app = require('./app')
 const cors = require('cors')
-// app.use(cors())
 const server = http.createServer(app)
-// const io = require('socket.io')(server, {
-//     cors: {
-//       origin: "http://localhost:8082",
-//       methods: ["GET", "POST"]
-//     },
-//     allowRequest: (req, callback) => {
-//         const noOriginHeader = req.headers.origin === undefined;
-//         callback(null, noOriginHeader);
-//     }
-//   });
 const io = require('socket.io')(server, {
     cors: {
-        // The `*` is used as the wildcard here.
         origin: process.env.URL_FRONTEND || 'http://localhost:8085',
-        // Set the other options according to your needs.
-        // The docs are here:
-        // https://www.npmjs.com/package/cors#configuration-options
         methods: ["GET", "POST"],
         allowedHeaders: ["content-type"],
         credentials: true
     }
 })
-
-// console.log('why do can this')
 
 const { API_PORT } = process.env
 const port = process.env.PORT || API_PORT
@@ -34,16 +17,6 @@ const port = process.env.PORT || API_PORT
 server.listen(port, () => {
     console.log(`Server runing at http://localhost:${port}`)
 })
-
-
-// io.set('transports', [
-//     'polling'
-// ])
-  
-// io.origins([
-//   'http://localhost:8082',
-// //   'https://entermeme.com',
-// ])
 
 io.on('connection', (socket) => {
     console.log(`User connected: ${socket.id}`);
